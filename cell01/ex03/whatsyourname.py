70120150dc6d1a779c09fcc76bacb40c6b5ef6d5
npatha-yy <67011194@kmitl.ac.th>
2025-06-24 15:20:33 +0700

    Create whatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,11 @@
+print("What's your first name? :")
+first_name = input()
+print(first_name)
+
+print("What's  your last name? :")
+last_name = input()
+print(last_name)
+
+print("What's  your full name? :")
+full_name =input()
+print(full_name)
