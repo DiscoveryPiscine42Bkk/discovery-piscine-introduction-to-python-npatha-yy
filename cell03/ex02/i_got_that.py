5bdf2f45889eeeffba9f01d9158ae59218732c93
npatha-yy <67011194@kmitl.ac.th>
2025-06-25 07:55:03 +0700

    Update i_got_that.py

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,6 +1,6 @@
 i = input("What you gotta say? : ")
 # i = Hello (First Input)
-while i == "Hello":
+while i == i:
     x = input("I got that! Anything else? : ")
     if x == "STOP":
         break
