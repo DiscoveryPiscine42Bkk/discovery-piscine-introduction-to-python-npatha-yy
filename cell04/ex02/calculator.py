916702a8f489cb80b998efa12380cd9bdae57a7a
npatha-yy <67011194@kmitl.ac.th>
2025-06-25 09:15:48 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+i = int(input("Give me the first number: "))
+j = int(input("Give me the second number: "))
+print("Thank you!")
+a1= i + j
+a2 = i - j
+a3 = i // j
+a4 = i * j
+print(i, "+", j, "=", a1 )
+print(i, "-", j, "=", a2 )
+print(i, "/", j, "=", a3 )
+print(i, "*", j, "=", a4 )
