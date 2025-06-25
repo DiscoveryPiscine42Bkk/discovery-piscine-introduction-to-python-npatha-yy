4fcae94684d2f0b6605de944839ede9c5e500b83
npatha-yy <67011194@kmitl.ac.th>
2025-06-25 15:45:56 +0700

    Create play_with_arrays.py

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+original_array = [2,8,9,48,8,22,-12,2]
+new_array = []
+for x in original_array:
+    new_array.append(x+2)
+print("Original.array: ",original_array)
+print("New array: ", new_array)
