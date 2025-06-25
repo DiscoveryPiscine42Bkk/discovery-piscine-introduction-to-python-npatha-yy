081965fbdd13987f12e4d1c77c63d7c9d9ad993f
npatha-yy <67011194@kmitl.ac.th>
2025-06-25 09:00:38 +0700

    Create age.py

---
+++ cell04/ex01/age.py
@@ -0,0 +1,6 @@
+age = input("Please tell me your age: ")
+n = int(age)
+print(f"You are currently {n} years old.")
+print(f"In 10 years, you'll be {n+10} years old.")
+print(f"In 20 years, you'll be {n+20} years old.")
+print(f"In 30 years, you'll be {n+30} years old.")
