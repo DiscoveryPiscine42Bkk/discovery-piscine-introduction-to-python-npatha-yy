c9870b6883c711344e6049dfd27616cf9b27cc3d
npatha-yy <67011194@kmitl.ac.th>
2025-06-26 22:28:57 +0700

    Update main.py checkmate.py

--- rushes/ex00/main.py checkmate.py
+++ rushes/ex00/main.py checkmate.py
@@ -1,1 +1,11 @@
 
+def checkmate(board):
+    size = len(board)
+
+    king_row = -1
+    king_colum = -1
+    for i in range(size):
+        for j in range(size):
+            if board[i][j] == "k":
+                king_row = i
+                king_colum = j
