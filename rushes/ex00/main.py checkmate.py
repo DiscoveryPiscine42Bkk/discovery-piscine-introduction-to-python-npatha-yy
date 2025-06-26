19b2f7239a5e6c898299a7d09e3a3a24aab09843
npatha-yy <67011194@kmitl.ac.th>
2025-06-27 01:01:25 +0700

    Update main.py checkmate.py

--- rushes/ex00/main.py checkmate.py
+++ rushes/ex00/main.py checkmate.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 def checkmate(board):
     size = len(board)
 
@@ -7,39 +6,39 @@ def checkmate(board):
     king_colum = -1
     for i in range(size):
         for j in range(size):
-            if board[i][j] == "k":
+            if board[i][j] == "K":
                 king_row = i
                 king_colum = j
     if king_row == -1:
         print("Fail")
         return
 
-    pawn_attracks = [(-1,-1), (-1,1)]
-    for dr,dc in pawn_attracks:
-        r = king_row +dr
+    pawn_attracks = [(-1, -1), (-1, 1)]
+    for dr, dc in pawn_attracks:
+        r = king_row + dr
         c = king_colum + dc
-        if 0 <= r <size and 0 <= c <size:
-            if board[r][c] =="p":
+        if 0 <= r < size and 0 <= c < size:
+            if board[r][c] == "P":
                 print("Success")
                 return
 
-    diagonals = [(-1,-1), (-1,1), (1,-1), (1,1)]
+    diagonals = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
     for dr, dc in diagonals:
         r = king_row + dr
-        c = king_colum +dc
+        c = king_colum + dc
         while 0 <= r < size and 0 <= c < size:
             piece = board[r][c]
             if piece == ".":
                 r += dr
                 c += dc
                 continue
-        if piece == "B" or piece =="Q":
-            print("Success")
-        break
-        
-    lines = [(-1,-0), (1,0), (0,-1), (0,1)]
+            if piece == "B" or piece == "Q":
+                print("Success")
+            break
+
+    lines = [(-1, -0), (1, 0), (0, -1), (0, 1)]
     for dr, dc in lines:
-        r = king_row +dr
+        r = king_row + dr
         c = king_colum + dc
         while 0 <= r < size and 0 <= c < size:
             piece = board[r][c]
@@ -49,7 +48,7 @@ def checkmate(board):
                 continue
             if piece == "R" or piece == "Q":
                 print("Success")
-                return 
+                return
             break
-            
+
     print("Fail")
