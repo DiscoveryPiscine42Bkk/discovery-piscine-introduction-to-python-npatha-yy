12d8ca8ae7932fdad31ce3fe09a9b02337a5e14e
npatha-yy <67011194@kmitl.ac.th>
2025-06-26 08:58:25 +0700

    Create  string_are_arrays.py

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+x = sys.argv[1:]
+if len(x) != 1:
+    print("noe")
+else:
+    z =x[0]
+    y = 0
+    for char in z:
+        if char == "z":
+             print("z", end="")
+             y+=1
+    if y ==0:
+        print("none")
