c42d84d52c84dbd1efccce1146284e63e7904c43
npatha-yy <67011194@kmitl.ac.th>
2025-06-27 07:59:10 +0700

    Create checmate.py

---
+++ rushes/ex00/checmate.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+def checkmate(board):
+    size = len(board)
+
+    king_row = -1
+    king_colum = -1
+    for i in range(size):
+        for j in range(size):
+            if board[i][j] == "K":
+                king_row = i
+                king_colum = j
+    if king_row == -1:
+        print("Fail")
+        return
+
+    pawn_attracks = [(-1, -1), (-1, 1)]
+    for dr, dc in pawn_attracks:
+        r = king_row + dr
+        c = king_colum + dc
+        if 0 <= r < size and 0 <= c < size:
+            if board[r][c] == "P":
+                print("Success")
+                return
+
+    diagonals = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+    for dr, dc in diagonals:
+        r = king_row + dr
+        c = king_colum + dc
+        while 0 <= r < size and 0 <= c < size:
+            piece = board[r][c]
+            if piece == ".":
+                r += dr
+                c += dc
+                continue
+            if piece == "B" or piece == "Q":
+                print("Success")
+            break
+
+    lines = [(-1, -0), (1, 0), (0, -1), (0, 1)]
+    for dr, dc in lines:
+        r = king_row + dr
+        c = king_colum + dc
+        while 0 <= r < size and 0 <= c < size:
+            piece = board[r][c]
+            if piece == ".":
+                r += dr
+                c += dc
+                continue
+            if piece == "R" or piece == "Q":
+                print("Success")
+                return
+            break
+
+    print("Fail")
+
+board = [
+        ["R", ".", ".", "."],
+        [".", "K", ".", "."],
+        [".", ".", ".", "."],
+        [".", ".", ".", 'P']
+]
+checkmate(board)
